e83e9646177fa1f87748be7289618882e3cfc844
Mojtaba Dashtinejad <mojtaba@gmail.com>
2018-12-31 09:53:19 +0330

    make RegateSwitch independence plugins

--- dist/regate.umd.js
+++ dist/regate.umd.js
@@ -1023,9 +1023,6 @@ exports.default = RegatePersianDateTimePicker;
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
 var RegateSwitch = {};
 
 RegateSwitch.init = function (_ref) {
@@ -1045,12 +1042,7 @@ RegateSwitch.init = function (_ref) {
 
   _input.name = name;
 
-  if ((typeof Switch === 'undefined' ? 'undefined' : _typeof(Switch)) === ( true ? 'undefined' : _typeof(undefined))) throw new Error('Switch plugin does not loaded in the page');
-
-  new Switch(_input, {
-    size: size,
-    checked: isChecked
-  });
+  if (isChecked) _input.checked = true;
 };
 
 RegateSwitch.Size = {
@@ -1059,7 +1051,7 @@ RegateSwitch.Size = {
   'Large': 'large'
 };
 
-RegateSwitch._markup = '\n  <input\n    id=\'{id}__input\'\n    type=\'checkbox\'\n    value=\'true\'\n  />\n';
+RegateSwitch._markup = '\n  <style>\n    #{id}__label {\n      position: relative;\n      margin-bottom: 0;\n      vertical-align: top;\n      display: inline-block;\n    }\n    \n    #{id}__label::before {\n      pointer-events: all;\n      border-radius: .5rem;\n      transition: background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;\n      position: absolute;\n      top: 0;\n      left: 0;\n      display: block;\n      width: 28px;\n      height: 16px;\n      content: "";\n      background-color: #fff;\n      border: #adb5bd solid 1px;\n      box-sizing: border-box;\n    }\n    \n    #{id}__label::after {\n      top: 2px;\n      left: 2px;\n      width: 12px;\n      height: 12px;\n      background-color: #adb5bd;\n      border-radius: .5rem;\n      transition: background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out,-webkit-transform .15s ease-in-out;\n      transition: transform .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;\n      transition: transform .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out,-webkit-transform .15s ease-in-out;\n      box-sizing: border-box;\n      position: absolute;\n      display: block;\n      content: "";\n      background-repeat: no-repeat;\n      background-position: center center;\n      background-size: 50% 50%;\n    }\n    \n    #{id}__input {\n      position: absolute;\n      z-index: -1;\n      width: 0;\n      height: 0;\n      overflow: hidden;\n      pointer-events: none;\n    }\n    \n    #{id}__input:checked~#{id}__label::before {\n      color: #fff;\n      border-color: #007bff;\n      background-color: #007bff;\n    }\n    \n    #{id}__input:checked~#{id}__label::after {\n      background-color: #fff;\n      -webkit-transform: translateX(.75rem);\n      transform: translateX(.75rem);\n    }\n    \n    #{id}__input:focus~#{id}__label::before {\n      box-shadow: 0 0 0 0.2rem rgba(0,123,255,.25);\n    }\n    \n    #{id}__input:checked~#{id}__label::before {\n      color: #fff;\n      border-color: #007bff;\n      background-color: #007bff;\n    }\n  </style>\n\n  <input\n    id=\'{id}__input\'\n    type=\'checkbox\'\n    value=\'true\'\n    tabindex="-1"\n  />\n\n  <label\n    id=\'{id}__label\'\n    for=\'{id}__input\'\n  ></label>\n';
 
 RegateSwitch.markup = function (id) {
   return RegateSwitch.getMarkup().replace(/{id}/g, id);
--- src/RegateSwitch.js
+++ src/RegateSwitch.js
@@ -17,13 +17,8 @@ RegateSwitch.init = function ({
 
   _input.name = name
 
-  if (typeof Switch === typeof undefined)
-    throw new Error('Switch plugin does not loaded in the page')
-
-  new Switch(_input, {
-    size: size,
-    checked: isChecked
-  })
+  if (isChecked)
+    _input.checked = true
 }
 
 RegateSwitch.Size = {
@@ -33,11 +28,92 @@ RegateSwitch.Size = {
 }
 
 RegateSwitch._markup = `
+  <style>
+    #{id}__label {
+      position: relative;
+      margin-bottom: 0;
+      vertical-align: top;
+      display: inline-block;
+    }
+    
+    #{id}__label::before {
+      pointer-events: all;
+      border-radius: .5rem;
+      transition: background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+      position: absolute;
+      top: 0;
+      left: 0;
+      display: block;
+      width: 28px;
+      height: 16px;
+      content: "";
+      background-color: #fff;
+      border: #adb5bd solid 1px;
+      box-sizing: border-box;
+    }
+    
+    #{id}__label::after {
+      top: 2px;
+      left: 2px;
+      width: 12px;
+      height: 12px;
+      background-color: #adb5bd;
+      border-radius: .5rem;
+      transition: background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out,-webkit-transform .15s ease-in-out;
+      transition: transform .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+      transition: transform .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out,-webkit-transform .15s ease-in-out;
+      box-sizing: border-box;
+      position: absolute;
+      display: block;
+      content: "";
+      background-repeat: no-repeat;
+      background-position: center center;
+      background-size: 50% 50%;
+    }
+    
+    #{id}__input {
+      position: absolute;
+      z-index: -1;
+      width: 0;
+      height: 0;
+      overflow: hidden;
+      pointer-events: none;
+    }
+    
+    #{id}__input:checked~#{id}__label::before {
+      color: #fff;
+      border-color: #007bff;
+      background-color: #007bff;
+    }
+    
+    #{id}__input:checked~#{id}__label::after {
+      background-color: #fff;
+      -webkit-transform: translateX(.75rem);
+      transform: translateX(.75rem);
+    }
+    
+    #{id}__input:focus~#{id}__label::before {
+      box-shadow: 0 0 0 0.2rem rgba(0,123,255,.25);
+    }
+    
+    #{id}__input:checked~#{id}__label::before {
+      color: #fff;
+      border-color: #007bff;
+      background-color: #007bff;
+    }
+  </style>
+
   <input
     id='{id}__input'
     type='checkbox'
     value='true'
+    tabindex="-1"
   />
+
+  <label
+    id='{id}__label'
+    for='{id}__input'
+  ></label>
 `
 
 RegateSwitch.markup = id => {
